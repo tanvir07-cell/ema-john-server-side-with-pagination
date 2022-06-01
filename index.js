e497ca01b095c4921544a36b19eb28e1aec77f62
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2022-06-01 23:49:13 +0700

    for crud's read complete

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// middleware:
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yhen6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emaJohn").collection("user");
+
+    // load all data from the mongodb backend:
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Ema john server side is ready");
+});
+
+app.listen(port, () => {
+  console.log(`Ema john is runnig to the port ${port}`);
+});
